cf0d204de25dd65deb2e15f961cef4b69cf3f47c
sadanand05 <85860328+sadanand05@users.noreply.github.com>
2021-09-08 16:42:28 +1000

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-// jest.config.js
-const {defaults} = require('jest-config');
-module.exports = {
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'js'],
-  roots: ["<rootDir>/__tests__/"]
-};
